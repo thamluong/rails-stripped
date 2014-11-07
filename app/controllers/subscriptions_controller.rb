2df18e6879d072350d3cc00f34c8981901e5859d
Bala Paranj <bala.paranj@zepho.com>
2014-11-07 01:38:41 -0800

    replaced hard-coded plan with a plan page for user selection of plan to signup

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,12 +1,15 @@
 class SubscriptionsController < ApplicationController
   def new
+    @subscription = Subscription.new(params.permit(:plan_name))    
   end
   
   def create
-    stripe = StripeGateway.new(Rails.logger)
+    stripe = StripeGateway.new(logger)
     # TODO : Assumption : After integration with user authentication gem like Devise, replace
-    # the hard-coded dummy value 'current_user.email'
-    @subscription = stripe.create_subscription('current_user.email', params[:stripeToken], 'gold')
+    # the hard-coded dummy value 'current_user.email'        
+    @subscription = stripe.create_subscription('current_user.email', params[:stripeToken], params[:plan_name])
   end
   
+  def pricing
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'subscriptions/new'
+  get 'subscriptions/new' => 'subscriptions#new', as: :new_subscription
   post 'subscriptions/create' => 'subscriptions#create'
   
+  get 'pricing' => 'subscriptions#pricing'
+    
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
