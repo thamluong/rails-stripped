5ab7e59778c5b65ab0f45d8240b047605c42019e
Bala Paranj <bala.paranj@zepho.com>
2014-11-14 13:40:37 -0800

    extracted helper to eliminate duplication in integration tests

--- spec/features/subscribe_spec.rb
+++ spec/features/subscribe_spec.rb
@@ -6,60 +6,40 @@ feature 'Subscription' do
   scenario 'Customer subscribes to Gold plan', js: true do
     sign_up('test@example.com', '12345678') 
 
-    click_link 'Gold'
-    fill_in "Card Number", with: '4242424242424242'
-    page.select '10', from: "card_month"
-    page.select '2029', from: 'card_year'
-     
-    click_button 'Subscribe Me'
+    subscribe_to_a_plan('Gold', '4242424242424242')
+    
     expect(page).to have_content('You have been subscribed to Gold.')
   end
   
   scenario 'Customer credit card expired', js: true do
     sign_up('test@example.com', '12345678') 
-        
-    click_link 'Gold'
-    fill_in "Card Number", with: '4000000000000069'
-    page.select '10', from: "card_month"
-    page.select '2029', from: 'card_year'
     
-    click_button 'Subscribe Me'
+    subscribe_to_a_plan('Gold', '4000000000000069')
+        
     expect(page).to have_content('Your card has expired.')
   end
   
   scenario 'Customer credit card number incorrect', js: true do
     sign_up('test@example.com', '12345678') 
     
-    click_link 'Gold'
-    fill_in "Card Number", with: '4242424242424241'
-    page.select '10', from: "card_month"
-    page.select '2029', from: 'card_year'
+    subscribe_to_a_plan('Gold', '4242424242424241')
     
-    click_button 'Subscribe Me'
     expect(page).to have_content('Your card number is incorrect.')    
   end
 
   scenario 'Customer credit card declined', js: true do
     sign_up('test@example.com', '12345678') 
     
-    click_link 'Gold'
-    fill_in "Card Number", with: '4000000000000002'
-    page.select '10', from: "card_month"
-    page.select '2029', from: 'card_year'
+    subscribe_to_a_plan('Gold', '4000000000000002')
     
-    click_button 'Subscribe Me'
     expect(page).to have_content('Your card was declined.')
   end
 
   scenario 'Customer credit card processing error', js: true do
     sign_up('test@example.com', '12345678') 
     
-    click_link 'Gold'
-    fill_in "Card Number", with: '4000000000000119'
-    page.select '10', from: "card_month"
-    page.select '2029', from: 'card_year'
+    subscribe_to_a_plan('Gold', '4000000000000119')
     
-    click_button 'Subscribe Me'
     expect(page).to have_content('An error occurred while processing your card. Try again in a little bit.')
   end
   
--- spec/support/features/session_helpers.rb
+++ spec/support/features/session_helpers.rb
@@ -8,5 +8,15 @@ module Features
       
       click_button 'Sign up'
     end
+    
+    def subscribe_to_a_plan(plan, credit_card)
+      click_link plan
+      fill_in "Card Number", with: credit_card
+      page.select '10', from: "card_month"
+      page.select '2029', from: 'card_year'
+
+      click_button 'Subscribe Me'
+    end
+    
   end
 end
