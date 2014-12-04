c6d807a4646596b50e6343d1f49594fc0d53ffab
Bala Paranj <bala.paranj@zepho.com>
2014-12-03 23:12:28 -0800

    deleted unused code

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
-  
-  def new
-    super
-  end
-  
+    
   def create
     user = current_or_guest_user
     Actors::Customer::UseCases.register_for_an_account(user, params[:user][:email], params[:user][:password])
