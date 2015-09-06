890d2ba8415b686a008736692ff6bb89dabcc6d9
Bala Paranj <bala.paranj@zepho.com>
2015-09-05 17:53:53 -0700

    updated readme

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     @orders = []
+    
     if user_signed_in?
       @orders = current_user.orders if current_user.has_orders? 
     end
