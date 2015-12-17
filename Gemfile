6d85009eb04420a5ab33e9e87d7104588c50ffe2
Bala Paranj <bala.paranj@zepho.com>
2015-12-17 12:28:22 -0800

    Added email specs using email_spec gem

--- Gemfile
+++ Gemfile
@@ -40,4 +40,5 @@ group :test do
   gem 'simplecov', :require => false
   gem 'vcr'
   gem 'webmock'
+  gem 'email_spec'
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,8 +1,35 @@
 require "rails_helper"
 
 describe UserMailer, :type => :mailer do
-  it 'has from@your-domain.com as the from email address'
-  it 'has user email as the to email address'
-  it 'has Payment to RubyPlus Failed as the subject of the email'  
-  it 'has link to edit credit card url in the email'
+  it 'has from@your-domain.com as the from email address' do
+    user = User.new(email: 'bugs@rubyplus.com')
+    
+    email = UserMailer.subscription_payment_failed(user).deliver_now
+
+    expect(email.from).to eq(['from@your-domain.com'])
+  end
+  
+  it 'delivers email to the correct email recepient' do
+    user = User.new(email: 'bugs@rubyplus.com')
+    
+    email = UserMailer.subscription_payment_failed(user).deliver_now
+    
+    expect(email).to deliver_to("bugs@rubyplus.com")
+  end
+    
+  it 'has Payment to RubyPlus Failed as the subject of the email' do
+    user = User.new(email: 'bugs@rubyplus.com')
+    
+    email = UserMailer.subscription_payment_failed(user).deliver_now
+
+    expect(email.subject).to eq('Payment to RubyPlus Failed')
+  end
+  
+  it 'has link to edit credit card url in the email' do
+    user = User.new(email: 'bugs@rubyplus.com')
+    
+    email = UserMailer.subscription_payment_failed(user).deliver_now
+
+    expect(email).to have_body_text(/#{credit_cards_edit_url}/)
+  end
 end
